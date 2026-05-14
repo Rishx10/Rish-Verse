dc7f02c1db7a370e2cb99c1ba791f0a9dcb898ed
Rish Verse <aurora.me404@gmail.com>
2026-05-14 20:25:44 +0530

    Final website rebuild

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Rish Verse Loaded Successfully");
+const galleryImages = document.querySelectorAll('.gallery-grid img');
+const lightbox = document.querySelector('.lightbox');
+const lightboxImg = document.querySelector('.lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+if(galleryImages){
+
+    galleryImages.forEach(img => {
+
+        img.addEventListener('click', () => {
+
+            lightbox.style.display = 'flex';
+            lightboxImg.src = img.src;
+
+        });
+
+    });
+
+}
+
+if(closeBtn){
+
+    closeBtn.addEventListener('click', () => {
+
+        lightbox.style.display = 'none';
+
+    });
+
+}
